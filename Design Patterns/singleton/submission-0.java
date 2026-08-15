2adb8f67a954e85c523622e515a5040d6687f784
Saketh Balijepalli <sakethb2@gmail.com>
2026-08-14 20:30:09 -0500

    Add: singleton - submission-0

---
+++ Design Patterns/singleton/submission-0.java
@@ -0,0 +1,26 @@
+static class Singleton {
+    private static Singleton instance = null;
+    private String value = null;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+}
+
+
